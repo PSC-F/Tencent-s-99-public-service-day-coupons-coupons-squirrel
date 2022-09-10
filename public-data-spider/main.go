d5ef945cdc3212dc899e6da0f0d6e50577e143b8
Pisces♓ <doudou843923050@gmail.com>
2022-09-10 13:45:54 +0800

    Create main.go
    创建 数据公开程序
    #完成基本封装
    #待接入reids
    #待实现页面可视化

---
+++ public-data-spider/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"strings"
+	"time"
+)
+
+var client http.Client
+
+const (
+	LOGIN_URL   = "https://alliance.yunzhanxinxi.com/login"
+	ContentType = "application/x-www-form-urlencoded"
+)
+
+func main() {
+	login()
+	getDashBoard()
+
+}
+
+// 获取后台看板数据
+func getDashBoard() {
+	c, l := getDate()
+	url := "https://alliance.yunzhanxinxi.com/order/list/statistics-info"
+	contentType := "application/x-www-form-urlencoded"
+	bodyStr := "channelId=127223&promotionId=&startTime=" + l.String() + "&endTime=" + c.String() + "&orderType="
+	t, _ := client.Post(url, contentType, strings.NewReader(bodyStr)) // 在这里登陆
+	defer t.Body.Close()
+	body, _ := ioutil.ReadAll(t.Body)
+	fmt.Println(string(body))
+}
+
+// 获取七天内数据时间
+func getDate() (time.Time, time.Time) {
+	currentTime := time.Now()
+	oldTime := currentTime.AddDate(0, 0, -7)
+	currentTime.Format("2006-01-02")
+	oldTime.Format("2006-01-02")
+	return currentTime, oldTime
+}
+
+// 登陆后台接口、缓存登陆信息
+func login() {
+	b := "" 
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		panic(err)
+	}
+	client.Jar = jar
+	t, _ := client.Post(LOGIN_URL, ContentType, strings.NewReader(b))
+	body, _ := ioutil.ReadAll(t.Body)
+	fmt.Println(string(body))
+}
